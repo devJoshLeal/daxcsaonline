638f1752db7efa76183d2cb7072c829e37ffe865
Joshua Leal Diaz <dev.joshualealdiaz@gmail.com>
2026-05-19 10:13:36 -0600

    Agregado /dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TokenController;
 Route::controller(App\Http\Controllers\WebController::class)->group(function () {
     Route::get('/', 'dashboard')->name('dashboard');
+    Route::get('/dashboard', 'dashboard')->name('dashboard_2');
     // Orders tab will be hidden if not logged in
     Route::get('/orders', 'orders')->name('orders')->middleware([
         'auth:sanctum'
