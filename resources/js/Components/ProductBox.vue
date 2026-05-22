80e892bb13271978f968441cf5ff0194104582e9
Joshua Leal Diaz <dev.joshualealdiaz@gmail.com>
2026-05-22 16:03:08 -0600

    Mejora visual en productbox

--- resources/js/Components/ProductBox.vue
+++ resources/js/Components/ProductBox.vue
@@ -3,16 +3,18 @@
         <div class="w-32 lg:w-48 cursor-pointer" @click.stop="toggleModal(showingProductModal)">
             <img :src="product.image" :alt="product.title ? product.title + ' cover' : 'product cover'"
                 class="">
-            <div class="h-5 bg-blue-400 text-white text-center">
-            </div>
         </div>
         <DialogModal :show="showingProductModal" :closeable="true" @close="toggleModal(showingProductModal)">
             <template #title>
-                {{ product.title || 'Product' }}
             </template>
 
             <template #content>
-                <div class="space-y-4">
+                <div class="space-y-4 max-w-md  mx-auto">
+                    <div class="text-lg text-center text-gray-600">
+                        <div v-if="product.platform" class="break-words">
+                            <span class="font-semibold text-gray-900">{{ product.title}}</span> 
+                        </div>
+                    </div>
                     <div v-if="product.image" class="flex justify-center">
                         <img :src="product.image" :alt="product.title ? product.title + ' cover' : 'product cover'"
                             class="w-48 rounded" />
@@ -32,7 +34,7 @@
                             <span class="font-semibold text-gray-900">Release Date</span> {{ product.release_date }}
                         </div>
                     </div>
-                    <div v-if="product.description" class="cursor-pointer text-xs text-gray-600"
+                    <div v-if="product.description" class="cursor-pointer text-justify text-xs text-gray-600"
                         @click="toggleDescription(showingFullDescription)">
                         {{ showingFullDescription ? product.description :
                             product.description.substring(0, 250) +"...(Click to expand)" }}
